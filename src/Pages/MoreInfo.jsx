f66f3ddfa40f87023b961b03ab2adf3287d09adf
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-03-07 02:51:18 +0530

    Moreinfo

--- src/Pages/MoreInfo.jsx
+++ src/Pages/MoreInfo.jsx
@@ -17,8 +17,8 @@ const MoreInfo = () => {
 
     useEffect(() => {
         const fetchSponsorDetails = async () => {
-            const token = localStorage.getItem('token')
-            console.log(token)
+            const token = localStorage.getItem('token');
+            console.log(token);
             try {
                 const response = await axios.post(`http://localhost:5000/api/sponsor/getSponsor/${id}`, {}, {
                     headers: {
@@ -26,8 +26,8 @@ const MoreInfo = () => {
                     }
                 });
                 if (response.data.success) {
-                    console.log(response.data)
-                    console.log(response.data.user)
+                    console.log(response.data);
+                    console.log(response.data.user);
 
                     setFormData(response.data.sponsor);
                     setuserdata(response.data.user);
@@ -57,7 +57,7 @@ const MoreInfo = () => {
         // Process form data here
         const token = localStorage.getItem('token');
         console.log(token);
-        console.log(formValues)
+        console.log(formValues);
         try {
             const response = await axios.post('http://localhost:5000/api/application/apply', formValues, {
                 headers: {
@@ -98,41 +98,39 @@ const MoreInfo = () => {
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className='flex border h-96 w-96 justify-center items-center'>
-                <div className="w-full max-w-lg bg-white shadow-md rounded-lg overflow-hidden">
-                    <div className="p-6">
-                        <div className="mb-4 border-b border-gray-300">
-                            <span className="text-gray-700">Name:</span>
-                            <div className="mt-1 font-semibold">{userdata.userName}</div>
-                        </div>
-                        <div className="mb-4 border-b border-gray-300">
-                            <span className="text-gray-700">Industry:</span>
-                            <div className="mt-1 font-semibold">{formData.industry}</div>
-                        </div>
-                        <div className="mb-4 border-b border-gray-300">
-                            <span className="text-gray-700">Description:</span>
-                            <div className="mt-1">{formData.description}</div>
-                        </div>
-                        <div className="mb-4 border-b border-gray-300">
-                            <span className="text-gray-700">Location:</span>
-                            <div className="mt-1 font-semibold">{formData.location}</div>
-                        </div>
-                        <div className="mb-4 border-b border-gray-300">
-                            <span className="text-gray-700">Budget:</span>
-                            <div className="mt-1 font-semibold">{formData.budget}</div>
-                        </div>
-                        <div className="mb-4 border-b border-gray-300">
-                            <span className="text-gray-700">Email:</span>
-                            <div className="mt-1 font-semibold">{formData.contact_email}</div>
-                        </div>
-                        <div className="mb-4">
-                            <span className="text-gray-700">Contact Number:</span>
-                            <div className="mt-1 font-semibold">{formData.contact_phone}</div>
-                        </div>
-                        <button onClick={handleApplyButtonClick} className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-lg'>
-                            Apply
-                        </button>
+            <div className="w-96  bg-white shadow-md rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
+                <div className="p-6">
+                    <div className="mb-4">
+                        <span className="text-gray-700 font-semibold underline">Name:</span>
+                        <div className="mt-1">{userdata.userName}</div>
                     </div>
+                    <div className="mb-4">
+                        <span className="text-gray-700 font-semibold underline">Industry:</span>
+                        <div className="mt-1">{formData.industry}</div>
+                    </div>
+                    <div className="mb-4">
+                        <span className="text-gray-700 font-semibold underline">Description:</span>
+                        <div className="mt-1">{formData.description}</div>
+                    </div>
+                    <div className="mb-4">
+                        <span className="text-gray-700 font-semibold underline">Location:</span>
+                        <div className="mt-1">{formData.location}</div>
+                    </div>
+                    <div className="mb-4">
+                        <span className="text-gray-700 font-semibold underline">Budget:</span>
+                        <div className="mt-1">{formData.budget}</div>
+                    </div>
+                    <div className="mb-4">
+                        <span className="text-gray-700 font-semibold underline">Email:</span>
+                        <div className="mt-1">{formData.contact_email}</div>
+                    </div>
+                    <div className="mb-4">
+                        <span className="text-gray-700 font-semibold underline">Contact Number:</span>
+                        <div className="mt-1">{formData.contact_phone}</div>
+                    </div>
+                    <button onClick={handleApplyButtonClick} className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-lg'>
+                        Apply
+                    </button>
                 </div>
             </div>
             {showApplyForm && (
@@ -144,23 +142,23 @@ const MoreInfo = () => {
                         <h2 className="text-2xl mb-4">Apply</h2>
                         <form onSubmit={handleFormSubmit}>
                             <div className="mb-4">
-                                <label htmlFor="reason" className="block text-gray-700">Reason:</label>
+                                <label htmlFor="reason" className="block text-gray-700 font-semibold">Reason:</label>
                                 <input type="text" id="reason" name="reason" className="mt-1 p-2 border rounded-md w-full" required onChange={handleInputChange} value={formValues.reason} />
                             </div>
                             <div className="mb-4">
-                                <label htmlFor="location" className="block text-gray-700">Location:</label>
+                                <label htmlFor="location" className="block text-gray-700 font-semibold">Location:</label>
                                 <input type="text" id="location" name="location" className="mt-1 p-2 border rounded-md w-full" required onChange={handleInputChange} value={formValues.location} />
                             </div>
                             <div className="mb-4">
-                                <label htmlFor="money" className="block text-gray-700">Money:</label>
+                                <label htmlFor="money" className="block text-gray-700 font-semibold">Money:</label>
                                 <input type="text" id="money" name="money" className="mt-1 p-2 border rounded-md w-full" required onChange={handleInputChange} value={formValues.money} />
                             </div>
                             <div className="mb-4">
-                                <label htmlFor="driveLink" className="block text-gray-700">Drive Link:</label>
+                                <label htmlFor="driveLink" className="block text-gray-700 font-semibold">Drive Link:</label>
                                 <input type="text" id="link" name="link" className="mt-1 p-2 border rounded-md w-full" required onChange={handleInputChange} value={formValues.link} />
                             </div>
                             <div className="mb-4">
-                                <label htmlFor="photoUpload" className="block text-gray-700">Photo Upload:</label>
+                                <label htmlFor="photoUpload" className="block text-gray-700 font-semibold">Photo Upload:</label>
                                 <input type="file" id="photoUpload" name="photoUpload" className="mt-1 p-2 border rounded-md w-full" onChange={handleInputChange} />
                             </div>
                             <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-lg">Submit</button>
